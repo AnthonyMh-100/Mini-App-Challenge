dcdbda1c751bdd79721fdb37d4781d24827a8db0
AnthonyMh-100 <amh-100@hotmail.com>
2026-01-08 10:58:32 -0500

    feat: add new constants

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,7 @@
 export const API_URL = "https://dummyjson.com/products";
+export const DEBOUNCE_DELAY = 3000;
+export const DEFAULT_PAGE = 1;
 export const LIMIT = 12;
+export const TEXT_LOADING = "Cargando...";
+
+export default { API_URL, DEBOUNCE_DELAY, DEFAULT_PAGE, LIMIT, TEXT_LOADING };
